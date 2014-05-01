b567ebba7b80056d2fdf9fe881e5378f981e25d6
Mariusz K Sasinski <m.sasinski@stormbyte.com>
2014-05-01 11:18:44 -0400

    Removed refs to other platforms

--- src/owWorldSimulation.h
+++ src/owWorldSimulation.h
@@ -1,17 +1,7 @@
 #ifndef OW_WORLD_SIMULATION_H
 #define OW_WORLD_SIMULATION_H
-
-#if defined(_WIN32) || defined (_WIN64)
-	#include <GL/glew.h>
-	#include <GL/wglew.h>
-#else
-	#include <string.h>
-#endif
-#if defined(__APPLE__) || defined(MACOSX)
-    #include <GLUT/glut.h>
-#else
-    #include <GL/freeglut.h>
-#endif
+#include <string.h>
+#include <GL/freeglut.h>
 
 #include "owPhysicsFluidSimulator.h"
 #include "VectorMath.h"
